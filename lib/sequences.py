05100a6abbeff89b44304e96a66f562e9e8cb379
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-08-28 20:59:50 +0300

    fibonacci sequnce

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        return
+    num1, num2 = 0, 1
+    count = 1
+    fib_sequence = [num1]
+    while count < length:
+        count += 1
+        num1, num2 = num2, num1 + num2
+        fib_sequence.append(num1)
+    print(fib_sequence)
